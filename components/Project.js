7e76b584c130d25c686a6e969343029d5ec524d5
Marc Terre <marc.terre@outlook.de>
2023-01-20 12:51:59 +0100

    add project component,  move the rendered list

---
+++ components/Project.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { titles } from "../lib/data.json";
+import { v4 as uuidv4 } from "uuid";
+import ToolsIcon from "../public/tools.svg";
+
+export default function Project() {
+  return (
+    <Ul>
+      {titles.map((title) => (
+        <Li key={uuidv4()}>
+          <StyledToolsIcon />
+          {title.name}
+        </Li>
+      ))}
+    </Ul>
+  );
+}
+
+const Ul = styled.ul`
+  list-style: none;
+`;
+
+const Li = styled.li`
+  border: 1px solid black;
+  width: 80vw;
+  margin: 10px;
+`;
+
+const StyledToolsIcon = styled(ToolsIcon)`
+  width: 25px;
+  height: 25px;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import { titles } from "../lib/data.json";
 import styled from "styled-components";
-import { v4 as uuidv4 } from "uuid";
-import ToolsIcon from "../public/tools.svg";
+import Project from "@/components/Project";
 
 export default function HomePage() {
   return (
@@ -10,14 +8,7 @@ export default function HomePage() {
         <H1>Your Projects</H1>
       </header>
       <main>
-        <Ul>
-          {titles.map((title) => (
-            <Li key={uuidv4()}>
-              <StyledToolsIcon />
-              {title.name}
-            </Li>
-          ))}
-        </Ul>
+        <Project />
       </main>
     </>
   );
@@ -26,19 +17,4 @@ export default function HomePage() {
 const H1 = styled.h1`
   position: relative;
   top: 0;
-`;
-
-const Ul = styled.ul`
-  list-style: none;
-`;
-
-const Li = styled.li`
-  border: 1px solid black;
-  width: 80vw;
-  margin: 10px;
-`;
-
-const StyledToolsIcon = styled(ToolsIcon)`
-  width: 25px;
-  height: 25px;
 `;
