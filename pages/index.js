e1feb7ddf44c9771354e5ea1ad1f62d1e7683a14
Marc Terre <marc.terre@outlook.de>
2023-01-20 11:54:59 +0100

    change title into name

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,25 @@
 import { titles } from "../lib/data.json";
+import styled from "styled-components";
+import { v4 as uuidv4 } from "uuid";
 
 export default function HomePage() {
   return (
     <>
       <header>
-        <h1>Your Projects</h1>
+        <H1>Your Projects</H1>
       </header>
       <main>
         <ul>
           {titles.map((title) => (
-            <li key={crypto.randomUUID()}>{title.title}</li>
+            <li key={uuidv4()}>{title.name}</li>
           ))}
         </ul>
       </main>
     </>
   );
 }
+
+const H1 = styled.h1`
+  position: relative;
+  top: 0;
+`;
