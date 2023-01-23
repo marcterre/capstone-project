3a44c1096ee2426593b2fe9300c243913d8e6fd6
Marc Terre <marc.terre@outlook.de>
2023-01-23 17:01:03 +0100

    add basic styling

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
 import styled from "styled-components";
 import ProjectItem from "@/components/ProjectItem";
+import { myProjectsContext } from "@/pages/_app";
+import { useContext } from "react";
 
 export default function HomePage() {
+  const currentProject = useContext(myProjectsContext);
+
   return (
     <>
       <header>
         <H1>Your Projects</H1>
       </header>
       <main>
-        <ProjectItem />
+        {currentProject ? (
+          <EmptyProjectPage>
+            Click + in ne navigation below to create a new project
+          </EmptyProjectPage>
+        ) : (
+          <ProjectItem />
+        )}
       </main>
     </>
   );
@@ -18,4 +28,9 @@ const H1 = styled.h1`
   position: relative;
   top: 0;
   margin: 10px;
+`;
+
+const EmptyProjectPage = styled.p`
+  padding: 10px;
+  text-align: center;
 `;
