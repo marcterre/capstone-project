e8ecb5913bf4f92a77c4c89574890340cb7a4b38
Marc Terre <marc.terre@outlook.de>
2023-01-23 10:03:42 +0100

    add basic form styling

--- pages/create-new-project/index.js
+++ pages/create-new-project/index.js
@@ -7,25 +7,37 @@ export default function CreateNewProject() {
   return (
     <>
       <header>
-        <h1>Create a new project</h1>
+        <H1>Create a new project</H1>
       </header>
       <main>
-        <form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
           <label htmlFor="projectName">Name:</label>
           <input id="projectName" name="projectName" type="text" />
-          <label htmlFor="projectDescription">Description</label>
+          <label htmlFor="projectDescription">Description:</label>
           <textarea id="projectDescription" name="projectDescription" />
           <label htmlFor="projectSketch">Add your sketch:</label>
           <input type="text" name="projectSketch" id="projectSketch" />
           <Button type="submit">Save</Button>
-        </form>
+        </Form>
       </main>
     </>
   );
 }
 
+const H1 = styled.h1`
+  margin: 10px;
+`;
+
 const Button = styled.button`
   position: fixed;
   bottom: 70px;
-  right: 40px;
+  right: 30px;
+  width: 100px;
+  height: 30px;
+`;
+
+const Form = styled.form`
+  display: grid;
+  padding: 5vw;
+  gap: 1vh;
 `;
