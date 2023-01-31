d6adee1c2196b42cff7106a01824477616238c86
Marc Terre <marc.terre@outlook.de>
2023-01-31 10:47:23 +0100

    add env local

---
+++ pages/api/upload.js
@@ -0,0 +1,14 @@
+import formidable from "formidable";
+import cloudinary from "cloudinary";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
